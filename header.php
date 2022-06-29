4370a77a0766840cece6cd9dfb854f50595b9a88
Al3Modo <alessio.modonesi@iisviolamarchesini.edu.it>
2022-06-29 11:18:57 +0200

    navbar sistemata

--- header.php
+++ header.php
@@ -18,54 +18,38 @@
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="nav-title">
 
                             <li class="nav-item dropdown" id="menu-1">
-                                <a class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Clienti&Specialisti</a>
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Gestiti&Specialisti</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="http://localhost/wordpress/cliente">Cliente</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/specialista">Specialista</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/gestito">Gestito</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/referente">Referente</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/specialista">Specialista</a></li>
                                 </ul>
                             </li>
 
                             <li class="nav-item dropdown" id="menu-2">
-                                <a class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Fascicoli</a>
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Fascicoli</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/fascicolo">Fascicolo</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/fascicolo-cliente">Fascicolo cliente</a>
-                                    </li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/fascicolo">Fascicolo</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/fascicolo-specialista">Fascicolo specialista</a></li>
                                 </ul>
                             </li>
 
                             <li class="nav-item dropdown" id="menu-3">
-                                <a class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Pagamenti</a>
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Pagamenti</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/pagamento">Pagamento</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/metodo-pagamento">Metodo di pagamento</a>
-                                    </li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/pagamento">Pagamento</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/metodo-pagamento">Metodo di pagamento</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/acconto">Acconto</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/rata">Rata</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/spese">Spese</a></li>
                                 </ul>
                             </li>
 
                             <li class="nav-item dropdown" id="menu-4">
-                                <a class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Altro</a>
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Altro</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/caso">Caso</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/stato">Stato</a>
-                                    </li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/caso">Caso</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/stato">Stato</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/ufficio">Ufficio</a></li>
                                 </ul>
                             </li>
