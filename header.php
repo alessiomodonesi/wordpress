82924d8796a067fafbbf3c7d8b430e0823164fc4
Al3Modo <alessio.modonesi@iisviolamarchesini.edu.it>
2022-06-29 12:07:13 +0200

    navbar

--- header.php
+++ header.php
@@ -23,7 +23,9 @@
                                     aria-expanded="false" id="nav-title">Gestiti&Specialisti</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/gestito">Gestiti</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/referente">Referenti</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/specialista">Specialisti</a></li>
                                 </ul>
                             </li>
@@ -32,6 +34,7 @@
                                 <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Fascicoli</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/fascicolo">Fascicoli</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/fascicolo-specialista">Fascicoli specialisti</a></li>
                                 </ul>
                             </li>
@@ -40,9 +43,13 @@
                                 <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Pagamenti</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/pagamento">Pagamenti</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/metodo-pagamento">Metodi di pagamento</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/acconto">Acconti</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/rata">Rate</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/spese">Spese</a></li>
                                 </ul>
                             </li>
@@ -51,7 +58,9 @@
                                 <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Altro</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/caso">Casi</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/stato">Stati</a></li>
+                                    <li><hr class="dropdown-divider"></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/ufficio">Uffici</a></li>
                                 </ul>
                             </li>
