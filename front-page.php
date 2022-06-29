2fb425ad337d8326e34a3ef7dff3e5880f8cc5be
Al3Modo <alessio.modonesi@iisviolamarchesini.edu.it>
2022-06-29 10:32:12 +0200

    home page

--- front-page.php
+++ front-page.php
@@ -3,4 +3,11 @@
 <div class="container-fluid"><br />
     <h1 class="title text-center">Home Page</h1>
     <input type="hidden" id="title" value="<?php echo get_the_title(); ?>"><hr />
+    <div class="row">
+        <div class="col-3"></div>
+            <div class="col-6">
+                <p><?php the_content() ?></p>
+            </div>
+        <div class="col-3"></div>
+    </div>
 </div>
