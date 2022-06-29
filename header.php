a12591fcd0f3a787d97ee43e1deb5f22a835d76b
Al3Modo <alessio.modonesi@iisviolamarchesini.edu.it>
2022-06-29 12:05:23 +0200

    header

--- header.php
+++ header.php
@@ -22,53 +22,40 @@
                                 <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
                                     aria-expanded="false" id="nav-title">Gestiti&Specialisti</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="http://localhost/wordpress/gestito">Gestito</a>
-                                    </li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/referente">Referente</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/specialista">Specialista</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/gestito">Gestiti</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/referente">Referenti</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/specialista">Specialisti</a></li>
                                 </ul>
                             </li>
 
                             <li class="nav-item dropdown" id="menu-2">
-                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false" id="nav-title">Fascicoli</a>
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Fascicoli</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/fascicolo">Fascicolo</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/fascicolo-specialista">Fascicolo
-                                            specialista</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/fascicolo">Fascicoli</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/fascicolo-specialista">Fascicoli specialisti</a></li>
                                 </ul>
                             </li>
 
                             <li class="nav-item dropdown" id="menu-3">
-                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false" id="nav-title">Pagamenti</a>
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Pagamenti</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/pagamento">Pagamento</a></li>
-                                    <li><a class="dropdown-item"
-                                            href="http://localhost/wordpress/metodo-pagamento">Metodo di pagamento</a>
-                                    </li>
-                                    <li><a class="dropdown-item" href="http://localhost/wordpress/acconto">Acconto</a>
-                                    </li>
-                                    <li><a class="dropdown-item" href="http://localhost/wordpress/rata">Rata</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/pagamento">Pagamenti</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/metodo-pagamento">Metodi di pagamento</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/acconto">Acconti</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/rata">Rate</a></li>
                                     <li><a class="dropdown-item" href="http://localhost/wordpress/spese">Spese</a></li>
                                 </ul>
                             </li>
 
                             <li class="nav-item dropdown" id="menu-4">
-                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false" id="nav-title">Altro</a>
+                                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="nav-title">Altro</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="http://localhost/wordpress/caso">Caso</a></li>
-                                    <li><a class="dropdown-item" href="http://localhost/wordpress/stato">Stato</a></li>
-                                    <li><a class="dropdown-item" href="http://localhost/wordpress/ufficio">Ufficio</a>
-                                    </li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/caso">Casi</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/stato">Stati</a></li>
+                                    <li><a class="dropdown-item" href="http://localhost/wordpress/ufficio">Uffici</a></li>
                                 </ul>
                             </li>
+                            
                         </ul>
                     </div>
                 </div>
